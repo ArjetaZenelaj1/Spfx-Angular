6bcf84a4a56c00b7b05fe56129547425dd48aa10
azenelaj <arjeta.zenelaj@eng.it>
2023-10-01 14:34:47 +0200

    init

---
+++ src/webparts/Events/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, Injector } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { createCustomElement } from "@angular/elements";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { AppComponent } from "./components/app.component";
+//import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
+
+import { DataService } from "./services/data.service";
+
+@NgModule({
+  imports: [BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule],
+  declarations: [AppComponent],
+  providers: [DataService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  public ngDoBootstrap() {
+    if (!customElements.get("spfx-app")) {
+      // define a custom element to get a valid entry point before Angular is fully bootstrapped
+      const appElement = createCustomElement(AppComponent, {
+        injector: this.injector,
+      });
+      customElements.define("spfx-app", appElement);
+    }
+  }
+}
